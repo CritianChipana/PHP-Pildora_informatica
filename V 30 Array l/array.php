1662bff86531b098a410e70a91a0645f870754c0
CritianChipana <cristianchipanahuaman@gmail.com>
2020-12-21 17:51:58 -0500

    Array indexsado y asociativo

---
+++ V 30 Array l/array.php
@@ -0,0 +1,46 @@
+<?php
+/* ARRAY INDEXADO **********************************************************************************/
+
+/* $semana[]="lunes";
+$semana[]="Martes";
+$semana[]="Jueves"; */
+
+/* $semana[1]="lunes";
+$semana[2]="Martes";
+$semana[3]="Jueves"; */
+/* echo $semana[1]; */
+
+/* $semana=array("lunes","martes","jueves");
+
+echo $semana[3]; */
+
+/* ARRAY ASOCIATIVO *********************************************************************************/
+/* $DATOS = array("nombre"=>"Juan","Apellido"=>"Chipana","edad"=>20);
+
+echo $DATOS['nombre'];
+ */
+
+$DATOS = array("nombre"=>"Juan","Apellido"=>"Chipana","edad"=>20);
+// AGREGAR UN ELEMENTO A UN ARREGLO ASOCIATIVO
+
+$DATOS ["DIA"]="lUNES";
+
+//COMPROBAR SI ES UN ARRAY
+/* if(is_array($DATOS)){
+    echo "Es un array";
+}else{
+    echo "No es array";
+} */
+// RECORRER UN ARREGLO 
+// foreach($DATOS as $clave=>$valor){
+//     echo "a $clave le corresponde $valor <br>";
+// }
+
+
+
+
+
+
+
+
+
---
+++ V 54  Marcadores en Consultas preparadas/formulario_busqueda.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscar Empleado con PDO</title>
+</head>
+<body>
+    
+    <form action="conexion.php" method="post">
+    
+        <label for="id">Digite ID:
+            <input type="text" name="id" id="id">
+        </label>
+        <input type="submit" value="Tocame!">
+    </form>
+
+</body>
+</html>
